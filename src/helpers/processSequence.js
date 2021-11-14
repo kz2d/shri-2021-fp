5fe25c8b186618d0e6e49aa38e6a368bb54a6d7a
kz2d <avidex111@gmail.com>
2021-11-15 00:04:56 +0300

    d

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,51 +1,85 @@
 /**
  * @file Домашка по FP ч. 2
- * 
+ *
  * Подсказки:
  * Метод get у инстанса Api – каррированый
  * GET / https://animals.tech/{id}
- * 
+ *
  * GET / https://api.tech/numbers/base
  * params:
  * – number [Int] – число
  * – from [Int] – из какой системы счисления
  * – to [Int] – в какую систему счисления
- * 
+ *
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
-import Api from '../tools/api';
+import {
+  allPass,
+  andThen,
+  curryN,
+  length,
+  pipe,
+  pipeWith,
+  prop,
+  split,
+  tap,
+  tryCatch,
+  __,
+} from "ramda";
+import Api from "../tools/api";
 
 const api = new Api();
-
+const toNum = (x) => +x;
+const apiNumbersBase = (num) =>
+  pipe(
+    api.get("https://api.tech/numbers/base", {
+      from: 10,
+      to: 2,
+      number: num,
+    }),
+    prop("result")
+  );
+const apiAnimalsId = (num) => api.get(`https://animals.tech/${num}/name`);
 /**
  * Я – пример, удали меня
  */
-const wait = time => new Promise(resolve => {
+const wait = (time) =>
+  new Promise((resolve) => {
     setTimeout(resolve, time);
-})
+  });
 
-const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
-
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  /**
+   * Я – пример, удали меня
+   */
+  api
+    .get("https://api.tech/numbers/base", { from: 10, to: 2, number: 10 })
+    .then((result) => {
+      console.log(result);
     });
+  const log = pipe(tap(console.log), tap(writeLog));
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
-
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
-
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
+  const validate = tryCatch(allPass([]), (err) => console.log("error" + err));
+  pipe(
+    log,
+    toNum,
+    Math.round,
+    log,
+    pipeWith(andThen)([
+      apiNumbersBase,
+      log,
+      split,
+      length,
+      log,
+      curryN(4, Math.pow)(__, 2),
+      log,
+      (x) => x % 3,
+      log,
+      apiAnimalsId,
+      handleSuccess,
+    ])
+  )(value);
+};
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,6 +1,24 @@
+import {
+  gt,
+  lte,
+  map,
+  sum,
+  whereEq,
+  composeK,
+  pipe,
+  values,
+  equals,
+  all,
+  and,
+  prop,
+  both,
+  allPass,
+  anyPass,
+  complement,
+} from "ramda";
 /**
  * @file Домашка по FP ч. 1
- * 
+ *
  * Основная задача — написать самому, или найти в FP библиотеках функции anyPass/allPass
  * Эти функции/их аналоги есть и в ramda и в lodash
  *
@@ -12,39 +30,82 @@
  *
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
+const eq =
+  (func) =>
+  (...args) =>
+    all(
+      equals(func[0](...args)),
+      func.map((a) => a(...args))
+    );
+const haveWhiteColor = whereEq("white");
+const haveGreenColor = whereEq("green");
+const haveBlueColor = whereEq("blue");
+const haveRedColor = whereEq("red");
+const haveOrangeColor = whereEq("orange");
+const getCircle = prop("circle");
+const getTriangle = prop("triangle");
+const getSqare = prop("square");
+const getStar = prop("star");
+const test = (a) => {
+  console.log(a);
+  return a;
+};
+const countByColor = (x) => pipe(values, map(x), sum);
+const countByColorGt = (x, y) => pipe(countByColor(x), lte(y));
 
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+export const validateFieldN1 = ({ star, square, triangle, circle }) => {
+  if (triangle !== "white" || circle !== "white") {
+    return false;
+  }
 
-    return star === 'red' && square === 'green';
+  return star === "red" && square === "green";
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = pipe(countByColorGt(haveGreenColor, 2));
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = eq([
+  countByColor(haveRedColor),
+  countByColor(haveBlueColor),
+]);
 
 // 4. Синий круг, красная звезда, оранжевый квадрат
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = allPass([
+  pipe(getCircle, haveBlueColor),
+  pipe(getStar, haveRedColor),
+  pipe(getSqare, haveOrangeColor),
+]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = anyPass([
+  countByColorGt(haveBlueColor, 3),
+  countByColorGt(haveGreenColor, 3),
+  countByColorGt(haveOrangeColor, 3),
+  countByColorGt(haveRedColor, 3),
+]);
 
 // 6. Две зеленые фигуры (одна из них треугольник), еще одна любая красная.
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = allPass([
+  pipe(countByColor(haveGreenColor), equals(2)),
+  pipe(countByColor(haveRedColor), equals(1)),
+  pipe(getTriangle, haveGreenColor),
+]);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = pipe(countByColorGt(haveOrangeColor, 4));
 
 // 8. Не красная и не белая звезда.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = complement(
+  anyPass([pipe(getStar, haveWhiteColor), pipe(getStar, haveRedColor)])
+);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = pipe(countByColorGt(haveGreenColor, 4));
 
 // 10. Треугольник и квадрат одного цвета (не белого)
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = allPass([
+  eq([getTriangle, getSqare]),
+  complement(pipe(getTriangle, haveWhiteColor)),
+]);
